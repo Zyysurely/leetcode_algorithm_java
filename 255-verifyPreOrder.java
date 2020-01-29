4bf503245fb05d531a3c106f95814b81ac4cc055
Zyysurely <zyysurely@163.com>
2020-01-30 00:52:34 +0800

    [feat]start

--- 255-verifyPreOrder.java
+++ 255-verifyPreOrder.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class Solution_255 {
     public boolean verifyPreorder(int[] preorder) {
         int len = preorder.length;
@@ -16,5 +18,24 @@ class Solution_255 {
             index++;
         }
         return helper(preorder, start+1,mid-1) && helper(preorder, mid, end);
+    }
+
+    // 单向栈
+    public boolean verifyPreorder_stack(int[] preorder) {
+        int len = preorder.length;
+        if(len < 2) return true;
+        Stack<Integer> stack = new Stack<>();
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<len; i++) {
+            if(preorder[i] < max) {
+                return false;
+            }
+            // 不停地加子树根进去
+            while(!stack.isEmpty() && preorder[i] > stack.peek()) {
+                max = stack.pop();
+            }
+            stack.push(preorder[i]);
+        }
+        return true;
     }
 }
