c1c9a5f02738ed64722a85ac6889a5a04b51ad51
Zyysurely <zyysurely@163.com>
2020-03-02 16:33:03 +0800

    [fea]剑指offer20

--- review-剑指offer/26-树的子结构.java
+++ review-剑指offer/26-树的子结构.java
@@ -1,4 +1,3 @@
-import apple.laf.JRSUIUtils.Tree;
 
 class TreeNode {
     TreeNode left;
@@ -18,6 +17,6 @@ class Solution_offer_26 {
         if(root1 == null) return false;
         if(root2 == null) return true;
         if(root1.val != root2.val) return false;
-        return isSubTree(root1.left, root2.left) && isSubTree(root1.right, root2.right;
+        return isSubTree(root1.left, root2.left) && isSubTree(root1.right, root2.right);
     }
 }
